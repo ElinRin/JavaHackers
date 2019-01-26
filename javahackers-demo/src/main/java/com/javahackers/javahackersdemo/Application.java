e23755906acbc0d2a4bec7efce74cb397c3ea7b9
PavelArzhaev <pavel.arzhaev@gmail.com>
2019-01-27 02:46:58 +0300

    DB generating attempts

--- javahackers-demo/src/main/java/com/javahackers/javahackersdemo/Application.java
+++ javahackers-demo/src/main/java/com/javahackers/javahackersdemo/Application.java
@@ -5,15 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 //@SpringBootApplication
 //@EnableJpaAuditing
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.project.ppaa.model"})
 public class Application {
 
     public static void main(String[] args) {
