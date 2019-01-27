7d53b801ddac8994ae6084ed55e49f4eb9e92a64
chernega25 <chernega_nikita@mail.ru>
2019-01-27 06:57:31 +0300

    hohoho

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -4,11 +4,12 @@ import {
     EMPLOYEE,
     COMPANY
 } from '../constants';
+import './styles.css';
 
 const Main = () => (
-    <div className='blockBorder'>
-        <Login user={EMPLOYEE}/>
+    <div className=''>
         <Login user={COMPANY}/>
+        <Login user={EMPLOYEE}/>
     </div>
 );
 
